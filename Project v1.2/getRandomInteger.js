e21b714b1939daa831cf9ae8b07adb0f9033cac7
DanielGreco <40527648+DanielGreco@users.noreply.github.com>
2018-07-05 13:29:10 -0400

    Add files via upload

---
+++ Project v1.2/getRandomInteger.js
@@ -0,0 +1,11 @@
+function getRandomInteger(lower,upper)
+{
+	if (lower>=upper)
+	{
+		return null;
+	}
+	var multiplier=upper-(lower-1);
+	var rnd=parseInt(Math.random()*multiplier);
+	rnd+=lower;
+	return rnd;
+}
